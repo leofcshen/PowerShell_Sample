d81fd3a20584bf115c843e465c32a8f4d63e2ea4
rovingwind <rovingwind93@gmail.com>
2023-06-22 15:33:36 +0800

    add

---
+++ Applications/BackupFolder/Backup_ABC.tw.ps1
@@ -0,0 +1,75 @@
+# 自訂備份位置
+$BackupBase = "D:\User\Desktop\Backup"
+
+Function Copy-WithProgress {
+	[CmdletBinding()]
+	Param (
+		[Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, Position = 0)]
+		$Source,
+		[Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, Position = 0)]
+		$Destination
+	)
+
+	$Source = $Source.tolower()
+	$FileList = Get-Childitem "$Source" -Recurse -Force
+	$Total = $FileList.count
+	$Position = 0
+
+	foreach ($File in $FileList) {
+		$Filename = $File.Fullname.tolower().replace($Source, '')
+		$DestinationFile = ($Destination + $Filename)
+		Write-Progress -Activity "Copying data from '$source' to '$Destination'" -Status "Copying File $Filename" -PercentComplete ($Position / $Total * 100)
+		Copy-Item $File.FullName -Destination $DestinationFile
+		$Position++
+	}
+}
+
+try {
+	1..9 | % { Write-Host }
+
+	$TargetFolerName = ((Get-Item $PSCommandPath).BaseName -split "_")[1]
+	$TargetFolerPath = "$PSScriptRoot\$TargetFolerName"
+	$Today = Get-Date -Format "yyyyMMdd"
+	$IsRunBackup = $true
+	$BackupFolderNewName = "${TargetFolerName}_${Today}"
+	$FolderList = (Get-ChildItem -Path $BackupBase -Directory -Filter "${BackupFolderNewName}*").Name
+
+	if ($FolderList.Count -ne 0) {
+		Write-Host "$BackupBase 已存在本日備份如下"
+
+		foreach ($Folder in $FolderList) {
+			Write-Host $Folder
+		}
+
+		Write-Host
+		Write-Host "是否仍要執行新的備份? Y/N(預設)"
+		$Key = $host.ui.RawUI.ReadKey("NoEcho,IncludeKeyUp")
+		$IsRunBackup = $Key.Character -eq "y"
+		$FolderNumber = ([int]($FolderList | ForEach-Object { $_.Replace("${TargetFolerName}_${Today}", 0).Replace("0_", "") } | Measure-Object -Max).Maximum) + 1
+		$BackupFolderNewName = "${BackupFolderNewName}_${FolderNumber}"
+		Write-Host
+	}
+
+	if ($IsRunBackup) {
+		$BackupDestination = "${BackupBase}\${BackupFolderNewName}"
+		Write-Host "備份開始"
+		Write-Host "來源 $TargetFolerPath"
+		Write-Host "目的 $BackupDestination"
+		Write-Host "備份中，請稍候…"
+
+		#Copy-Item -Path $TargetFolerPath -Destination $BackupDestination -Recurse -Force
+		Copy-WithProgress -Source $TargetFolerPath -Destination $BackupDestination
+
+		Write-Host "備份完成"
+	}
+ else {
+		Write-Host "已取消備份"
+	}
+}
+catch {
+	Write-Host $Error[0]
+}
+
+Write-Host
+Write-host "輸入任意鍵離開..."
+$host.ui.RawUI.ReadKey("NoEcho,IncludeKeyUp")
--- Class_物件.ps1
+++ Class_物件.ps1
@@ -1,20 +1,40 @@
-class Car {
-	[string] $Color;
+class Vehicle {
 	[string] $Name;
 
-	[int] $Length
-	[int] $Width
+	Vehicle() {
+		Write-Host "父層空建構函式" -ForegroundColor Green;
+	}
 
-	[string] $Manufacturer;
-	[string] $Model;
+	Vehicle([string] $Name) {
+		$this.Name = $Name;
+	}
+}
 
+# 繼承
+class Car : Vehicle {
 	[int] $Mileage;
+	[string] $Color;
+
+	Car() {
+		Write-Host "子層空建構函式" -ForegroundColor Red;
+	}
+	# 沒指定 Base($Name) 會呼父層空建構函式
+	Car([string] $Name) : Base($Name) {	}
+
+	Car([string] $Name, [int] $Mileage) : Base($Name) {
+		$this.Mileage = $Mileage;
+	}
 
 	[void] Drive([int] $Mileage) {
 		$this.Mileage += $Mileage
 	}
 }
 
-$MyCar = [Car]::new()
-$MyCar.Manufacturer = "Bwm"
+[Car]::new();
+[Car]::new('BMW');
+[Car]::new('Audi', 20000);
+
+$MyCar = [Car]::new('MyCar')
+$MyCar.Color = "Red"
 $MyCar.Drive(12)
+$MyCar
