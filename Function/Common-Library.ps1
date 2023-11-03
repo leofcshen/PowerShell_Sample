57e3866a7131dc59463f781316afdb07247cf15c
Leo from DT-6084 <rovingwind93@gmail.com>
2023-11-03 15:47:38 +0800

    update

--- Function/Common-Library.ps1
+++ Function/Common-Library.ps1
@@ -1,36 +1,20 @@
-﻿function Send-Notification {
-	param (
-		[string]$Title,
-		[string]$Text,
-		[string]$IconType = "Information"
-	)
-	# 加載 Windows UI 通知的必要組件
-	Add-Type -AssemblyName System.Windows.Forms
+﻿$IsVersion7 = $PSVersionTable.PSVersion.Major -eq 7
+$CommonBase = "$PSScriptRoot\Common"
+$FunctionBase7 = "$PSScriptRoot\Version_7"
+$FunctionBaseOld = "$PSScriptRoot\Version_Old"
 
-    # 創建通知物件
-	$balloon = New-Object System.Windows.Forms.NotifyIcon
-
-    # 設定圖示
-    switch ($IconType) {
-			"Warning" { $balloon.Icon = [System.Drawing.SystemIcons]::Warning }
-			"Error" { $balloon.Icon = [System.Drawing.SystemIcons]::Error }
-			"Question" { $balloon.Icon = [System.Drawing.SystemIcons]::Question }
-			"Hand" { $balloon.Icon = [System.Drawing.SystemIcons]::Hand }
-			default { $balloon.Icon = [System.Drawing.SystemIcons]::Information }
-    }
-
-    # 設定標題和內容
-	$balloon.BalloonTipTitle = $Title
-	$balloon.BalloonTipText = $Text
-    
-	# 顯示通知 
-	$balloon.Visible = $true
-	$balloon.ShowBalloonTip(0)
-    
-	# 延時後清除
-	Start-Sleep -Seconds 3
-	$balloon.Dispose()
+# region 依 PowerShell 版本引用方法
+if($IsVersion7) {
+	$FunctionBase = $FunctionBase7
+} else {
+	$FunctionBase = $FunctionBaseOld
 }
+
+Get-ChildItem $FunctionBase | % { . $_.FullName }
+# endregion
+
+# 引用通用方法
+Get-ChildItem $CommonBase | % { . $_.FullName}
 
 function MyPadRight {
 	param (
@@ -46,56 +30,6 @@ function MyPadRight {
 	$adjustedWidth = $TotalWidth - $chineseCharCount
 
 	return $InputString.PadRight($adjustedWidth, $PaddingChar)
-}
-
-function Test-HostPing {
-	param (
-		[PSCustomObject]$Item
-	)
-
-	$Result = Test-NetConnection -ComputerName $Item.IP -WarningAction SilentlyContinue
-	$formattedName = MyPadRight $Item.Name 20
-	$formattedIPPort = "$($Item.IP)".PadRight(26, ' ')
-	$status = ($Result.PingSucceeded) ? "成功" : "失敗"
-	$Color = ($Result.PingSucceeded) ? "Green" : "Red"
-	$message = ("$formattedName $formattedIPPort   " + "Ping Host".PadRight($ActionPadNumber, ' ') + " " + $status)
-	Write-Host $message -ForegroundColor $Color
-
-	return $Result.PingSucceeded
-}
-
-function Test-ConnectionAndPort {
-	param (
-		[PSCustomObject]$Item,
-		[string]$Action,
-		[int]$Port
-	)
-	
-	$Action = "Test Port > " + $Action
-	$Result = Test-NetConnection -ComputerName $Item.IP -Port $Port -WarningAction SilentlyContinue
-
-	$formattedIPPort = "$($Item.IP):$Port".PadRight(26, ' ')
-	$status = ($Result.TcpTestSucceeded) ? "成功" : "失敗"
-	$Color = ($Result.TcpTestSucceeded) ? "Green" : "Red"
-	$message = (" " * 21 + $formattedIPPort + "   " + $Action.PadRight($ActionPadNumber, ' ') + " " + $status)
-	Write-Host $message -ForegroundColor $Color
-
-	return $Result.TcpTestSucceeded
-}
-
-function Test-Http {
-	param (
-		[string]$Action,
-		[string]$url
-	)
-	
-	
-	$Action = "Test Http > " + $Action
-	$response = Invoke-WebRequest -Uri $url -Method Head -SkipHttpErrorCheck
-	$is200 = $response.StatusCode -eq 200
-	$urlTest = $Action.PadRight($ActionPadNumber, ' ') + " " + ($is200 ? "成功" : "失敗") + " " + $response.StatusCode
-	$urlTestColor = $is200 ? "Green" : "Red"
-	Write-Host (" " * 21 + $url.PadRight(28, ' ') + " " + $urlTest) -ForegroundColor $urlTestColor
 }
 
 function Check-IsRunTime {
@@ -117,18 +51,4 @@ function Check-IsRunTime {
 	}
 	
 	return $isRunTime
-}
-
-function GetConfig {
-	param (
-		[string]$ConfigPath
-	)
-	
-	$ConfigExist = Test-Path -Path $ConfigPath -PathType Leaf
-	# 引用設定檔
-	if (!$ConfigExist) { throw "$ConfigPath 設定檔不存在"	}
-	$Config = Get-Content -Path $ConfigPath | ConvertFrom-Json
-	# 引用腳本
-	#. $Config.BaseScript
-	return $Config
 }
