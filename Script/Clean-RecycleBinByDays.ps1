d74ae18526fba6279b949f574551fb0889ec415a
Leo from DT-6084 <rovingwind93@gmail.com>
2023-11-10 16:37:04 +0800

    update

---
+++ Script/Clean-RecycleBinByDays.ps1
@@ -0,0 +1,14 @@
+﻿# 刪除資源回收桶超過 n 天的檔案
+
+$days = $args[0]
+
+# 定義刪除時間
+$limit = (Get-Date).AddDays(-$days)
+# 獲取資源回收桶位置
+$recycle = (New-Object -ComObject Shell.Application).NameSpace(0xa)
+
+$recycle.Items() | ForEach-Object {
+	if($_.ExtendedProperty("System.Recycle.DateDeleted") -lt $limit){
+		Remove-Item $_.Path -Recurse -Force
+	}
+}
